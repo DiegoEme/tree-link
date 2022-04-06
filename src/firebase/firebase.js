d0f813b6ad2cd2e7ad88648534b019345c5328fd
unknown <soterg.uhdeia@gmail.com>
2022-04-06 08:07:38 -0500

    choose username view

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -50,4 +50,18 @@ const userExists = async (uid) => {
   return res.exists();
 };
 
-export { app, auth, db, storage, userExists };
+const existsUsername = async (username) => {
+  const users = [];
+  const docsRef = collection(db, "users");
+  const q = query(docsRef, where("username", "==", username));
+
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach((doc) => {
+    users.push(doc.data());
+  });
+
+  return users.length > 0 ? users[0].uid : null;
+};
+
+export { app, auth, db, storage, userExists, existsUsername };
--- src/routes/chooseUsernameView.jsx
+++ src/routes/chooseUsernameView.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-function ChooseUsernameView(props) {
-  return <div>choose user name</div>;
+import AuthProvider from "../components/authProvider";
+import { existsUsername } from "../firebase/firebase";
+
+function ChooseUsernameView() {
+  const [state, setState] = useState(0);
+  const [currentUser, setCurrentUser] = useState({});
+  const [username, setUsername] = useState("");
+
+  /* 
+  State
+  0: inicializado
+  1: loading
+  2: login completo
+  3: login pero sin registro
+  4: no hay nadie logueado
+  5: Ya existe username
+  */
+
+  const navigate = useNavigate();
+
+  const handleUserLoggedIn = () => {
+    navigate("/dashboard");
+  };
+
+  const handleUserNotLoggedIn = () => {
+    navigate("login");
+  };
+
+  const handleUserNotRegistered = (user) => {
+    setCurrentUser(user);
+    setState(3);
+  };
+
+  const handleInputUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleContinue = async () => {
+    if (username !== "") {
+      const exists = await existsUsername(username);
+
+      if (exists) {
+        setState(5);
+      } else {
+        const temp = { ...currentUser };
+        temp.processCompleted = true;
+      }
+    }
+  };
+
+  if (state === 3) {
+    return (
+      <div>
+        <h1>Bienvenido {currentUser.displayName}</h1>
+        <p>Para terminar el proceso elige un nombre de usuario</p>
+        <div>
+          <input type="text" onChange={handleInputUsername} />
+        </div>
+        <div>
+          <button onClick={handleContinue}>Continuar</button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <AuthProvider
+      onUserLoggedIn={handleUserLoggedIn}
+      onUserNotLoggedIn={handleUserNotLoggedIn}
+      onUserNotRegistered={handleUserNotRegistered}
+    ></AuthProvider>
+  );
 }
 
 export default ChooseUsernameView;
